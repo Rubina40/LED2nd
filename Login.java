b297b27392dbfc69e692f917f4dff2692afedade
rubina40 <rubinakarki318@gmail.com>
2021-09-25 23:28:37 +0545

    test

--- Login.java
+++ Login.java
@@ -6,6 +6,8 @@ import java.awt.Font;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -14,6 +16,10 @@ import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 
 public class Login extends JFrame implements ActionListener {
+	
+	private ImageIcon bg;
+	private JLabel myLabel;
+
 
 	JFrame fr;
 	JLabel lbl_heading,lbl_username,lbl_password;
@@ -23,61 +29,65 @@ public class Login extends JFrame implements ActionListener {
 	Font fon1,fon2;
 	
 	public Login() {
+		
+		bg = new ImageIcon(this.getClass().getResource("/customer_regis.png"));
+		myLabel = new JLabel(bg);
+		myLabel.setSize(450,400);
 	
 		fr=new JFrame("Login Page");
-		fr.getContentPane().setBackground(Color.pink);
+		fr.add(myLabel);
 		
 		
 		fon1=new Font("arial",Font.BOLD,20);
 		fon2=new Font("arial",Font.BOLD,16);
 		
 		lbl_heading=new JLabel("Login Credentials");
-		lbl_heading.setForeground(Color.black);
+		lbl_heading.setForeground(Color.pink);
 		lbl_heading.setFont(fon1);
 		lbl_heading.setBounds(170,10,200,30);
-		fr.add(lbl_heading);
+		myLabel.add(lbl_heading);
 		
 		lbl_username=new JLabel("Username");
-		lbl_username.setForeground(Color.blue);
+		lbl_username.setForeground(Color.yellow);
 		lbl_username.setFont(fon1);
 		lbl_username.setBounds(50,50,150,40);
-		fr.add(lbl_username);
+		myLabel.add(lbl_username);
 		
 		txt_username=new JTextField();
 		txt_username.setFont(fon2);
 		txt_username.setBounds(155, 50, 220, 40);
-		fr.add(txt_username);
+		myLabel.add(txt_username);
 		
 		lbl_password=new JLabel("password");
-		lbl_password.setForeground(Color.blue);
+		lbl_password.setForeground(Color.yellow);
 		lbl_password.setFont(fon1);
 		lbl_password.setBounds(50,100,100,40);
-		fr.add(lbl_password);
+		myLabel.add(lbl_password);
 		
 		txt_password=new JPasswordField();
 		txt_password.setFont(fon2);
 		txt_password.setBounds(155, 100, 220, 40);
-		fr.add(txt_password);
+		myLabel.add(txt_password);
 		
 		
 		btn_login=new JButton("login");
 		btn_login.setFont(fon2);
 		btn_login.setBounds(155, 150, 90, 40);
 		btn_login.addActionListener(this);
-		fr.add(btn_login);
+		myLabel.add(btn_login);
 		
 		btn_cancel=new JButton("Cancel");
 		btn_cancel.setFont(fon2);
 		btn_cancel.setBounds(250, 150, 90, 40);
 		btn_cancel.addActionListener(this);
-		fr.add(btn_cancel);
+		myLabel.add(btn_cancel);
 		
 		btn_reg=new JButton("Sign_up");
 		btn_reg.setForeground(Color.red);
 		btn_reg.setFont(fon2);
 		btn_reg.setBounds(320, 220, 100, 30);
 		btn_reg.addActionListener(this);
-		fr.add(btn_reg);
+		myLabel.add(btn_reg);
 		
 		fr.setSize(450,300);
 		fr.setLayout(null);
@@ -96,8 +106,16 @@ public class Login extends JFrame implements ActionListener {
 	}
 	@Override
 	public void actionPerformed(ActionEvent e) {
+		
+		 if((e.getSource()==btn_cancel)) {
+				
+			 System.exit(0);
+				
+		 }
 		if(e.getSource()==btn_reg) {
 			new Register();
+			
+			fr.setVisible(false);
 		}
 		
 		else if((e.getSource()==btn_login)){
@@ -105,22 +123,23 @@ public class Login extends JFrame implements ActionListener {
 			String usrname= txt_username.getText();
 			String pass= txt_password.getText();
 			
+			if(usrname.equals("") || pass.equals("")) {
+				
+				JOptionPane.showMessageDialog(fr,"Please fill both the fields!!");
+				
+			}
 			
-			if(usrname.equals("rubi") && pass.equals("rubi11")) {
+				else if((usrname.equals("rubi") && pass.equals("rubi11")) || (usrname.equals("ram") && pass.equals("ram7"))) {
 				
 				JOptionPane.showMessageDialog(fr,"Welcome you are logged in sucessfully");
 				
-		}else {
+				}else {
 			
-			JOptionPane.showMessageDialog(fr,"Sorry!!Ivalid Id or passwprd");
+					JOptionPane.showMessageDialog(fr,"Sorry!!Ivalid Id or passwprd");
 			
-		}
+				}
 		
-		if(e.getSource()==btn_cancel) {
-			
-			System.exit(0);
-		
-		}
+	
 	}
 }
 	
--- Register.java
+++ Register.java
@@ -5,6 +5,7 @@ import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -13,88 +14,104 @@ import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 
 public class Register extends JFrame implements ActionListener{
+	
+	
+	private ImageIcon bg;
+	private JLabel myLabel;
 
 	
 	JFrame fr;
 	JLabel lbl_heading,lbl_fname,lbl_lname,lbl_email,lbl_password,lbl_confirm;
 	JTextField txt_fname,txt_lname,txt_email;
 	JPasswordField txt_password,txt_confirm;
-	JButton btn_create;
+	JButton btn_create, btn_back;
 	Font fon1,fon2;
 	
 public Register() {
-	fr=new JFrame("Registration Page");
-	fr.getContentPane().setBackground(Color.pink);
 	
-	fon1=new Font("arial",Font.BOLD,24);
+	bg = new ImageIcon(this.getClass().getResource("/customer_regis.png"));
+	myLabel = new JLabel(bg);
+	myLabel.setSize(450,400);
+	
+	fr=new JFrame("Registration Page");
+	fr.add(myLabel);
+	
+	fon2=new Font("arial",Font.BOLD,22);
 	fon1=new Font("Times new roman",Font.BOLD,18);
+
 	
 	lbl_heading=new JLabel("Create Account");
-	lbl_heading.setForeground(Color.red);
-	lbl_heading.setFont(fon1);
+	lbl_heading.setForeground(Color.pink);
+	lbl_heading.setFont(fon2);
 	lbl_heading.setBounds(170,10,200,30);
-	fr.add(lbl_heading);
+	myLabel.add(lbl_heading);
 	
 	lbl_fname=new JLabel("Fullname:");
-	lbl_fname.setForeground(Color.blue);
+	lbl_fname.setForeground(Color.yellow);
 	lbl_fname.setFont(fon1);
 	lbl_fname.setBounds(50,50,150,40);
-	fr.add(lbl_fname);
+	myLabel.add(lbl_fname);
 	
 	txt_fname=new JTextField();
-	txt_fname.setFont(fon2);
+	txt_fname.setFont(fon1);
 	txt_fname.setBounds(200, 50, 200, 30);
-	fr.add(txt_fname);
+	myLabel.add(txt_fname);
 	
 	lbl_lname=new JLabel("Username:");
-	lbl_lname.setForeground(Color.blue);
+	lbl_lname.setForeground(Color.yellow);
 	lbl_lname.setFont(fon1);
 	lbl_lname.setBounds(50,90,150,40);
-	fr.add(lbl_lname);
+	myLabel.add(lbl_lname);
 	
 	txt_lname=new JTextField();
-	txt_lname.setFont(fon2);
+	txt_lname.setFont(fon1);
 	txt_lname.setBounds(200, 90, 200, 30);
-	fr.add(txt_lname);
+	myLabel.add(txt_lname);
 	
 	lbl_email=new JLabel("Email:");
-	lbl_email.setForeground(Color.blue);
+	lbl_email.setForeground(Color.yellow);
 	lbl_email.setFont(fon1);
 	lbl_email.setBounds(50,130,150,40);
-	fr.add(lbl_email);
+	myLabel.add(lbl_email);
 	
 	txt_email=new JTextField();
-	txt_email.setFont(fon2);
+	txt_email.setFont(fon1);
 	txt_email.setBounds(200, 130, 200, 30);
-	fr.add(txt_email);
+	myLabel.add(txt_email);
 	
 	lbl_password=new JLabel("password:");
-	lbl_password.setForeground(Color.blue);
+	lbl_password.setForeground(Color.yellow);
 	lbl_password.setFont(fon1);
 	lbl_password.setBounds(50,170,150,40);
-	fr.add(lbl_password);
+	myLabel.add(lbl_password);
 	
 	txt_password=new JPasswordField();
-	txt_password.setFont(fon2);
+	txt_password.setFont(fon1);
 	txt_password.setBounds(200, 170, 200, 30);
-	fr.add(txt_password);
+	myLabel.add(txt_password);
 	
 	lbl_confirm=new JLabel("Retype password:");
-	lbl_confirm.setForeground(Color.blue);
+	lbl_confirm.setForeground(Color.yellow);
 	lbl_confirm.setFont(fon1);
 	lbl_confirm.setBounds(50,210,170,40);
-	fr.add(lbl_confirm);
+	myLabel.add(lbl_confirm);
 	
 	txt_confirm=new JPasswordField();
-	txt_confirm.setFont(fon2);
+	txt_confirm.setFont(fon1);
 	txt_confirm.setBounds(200, 210, 200, 30);
-	fr.add(txt_confirm);
+	myLabel.add(txt_confirm);
 	
 	btn_create=new JButton("Create");
 	btn_create.setFont(fon2);
 	btn_create.setBounds(120, 270, 170, 40);
 	btn_create.addActionListener(this);
-	fr.add(btn_create);
+	myLabel.add(btn_create);
+	
+	btn_back=new JButton("back");
+	btn_back.setFont(fon1);
+	btn_back.setBounds(0, 0, 100, 30);
+	btn_back.addActionListener(this);
+	myLabel.add(btn_back);
 	
 	fr.setSize(450,400);
 	fr.setLayout(null);
@@ -114,16 +131,23 @@ public Register() {
 		String full_name=txt_fname.getText();
 		String username=txt_lname.getText();
 		String pass=txt_password.getText();
+		String email=txt_email.getText();
 		
 		
 		if(e.getSource()==btn_create) {
 			Dboperation db= new Dboperation();
-			String query = "insert into usr_tbl(full_name, username, pass) values('"+full_name+"','"+username+"', '"+pass+"')";
+			String query = "insert into usr_tbl(full_name, username, email, pass) values('"+full_name+"','"+username+"', '"+email+"', '"+pass+"')";
 			int ans=db.insert(query);
 			
 			if(ans>0) {
 				JOptionPane.showMessageDialog(fr, "user created sucessfully");
 			}
+		}
+		
+		if(e.getSource()==btn_back) {
+			
+			new Login();
+			fr.setVisible(false);
 		}
 	}
 }
